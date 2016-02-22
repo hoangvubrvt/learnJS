30f1089add6effdb3c6d1fc2d74e9250a6cdf72d
Hoàng Vũ <hoangvubrvt@gmail.com>
2016-02-23 00:03:37 +0700

    learn Object contructor in JS

--- js/objectdemo.js
+++ js/objectdemo.js
@@ -1,40 +1,35 @@
-var outerSpaceMovie = {
-  title : "Outer Space",
-  genre : "cult classis",
-  rating : 2,
-  showtimes : ["3:00 PM", "7:00 PM", "11:00 PM"]
-};
-
-var forbiddenPlanetMovie = {
-  title : "Forbidden Planet",
-  genre : "classis sci-fi",
-  rating : 5,
-  showtimes : ["5:00 PM", "9:00 PM"]
-};
-
-function getNextShowing(movie){
-  var now = new Date().getTime();
-  
-  for(var i = 0; i < movie.showtimes.length; i++){
-      var showTime = getTimeFromString(movie.showtimes[i]);
-      console.log(showTime);
-      if((showTime - now) > 0){
-          return "Next showing of "+movie.title + " is " + movie.showtimes[i];
-      }
-  }
-  return null;
-}
-
 function getTimeFromString(timeString){
     var theTime = new Date();
     var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
     theTime.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
     theTime.setMinutes(parseInt(time[2]) || 0);
     return theTime.getTime();
+};
+
+function Movie(title, genre, rating, showtimes){
+    this.title = title;
+    this.genre = genre;
+    this.rating = rating;
+    this.showtimes = showtimes;
+    this.getNextShowing = function() {
+      var now = new Date().getTime();
+
+      for(var i = 0; i < this.showtimes.length; i++){
+      var showTime = getTimeFromString(this.showtimes[i]);
+      if((showTime - now) > 0){
+          return "Next showing of "+this.title + " is " + this.showtimes[i];
+      }
+    }
+    return null;
+  };
 }
 
-var nextShowing = getNextShowing(outerSpaceMovie);
+var outerSpaceMovie = new Movie("Outer Space", "cult classis", 2, ["3:00 PM", "7:00 PM", "11:00 PM"]);
+
+var forbiddenPlanetMovie = new Movie("Forbidden Planet", "classis sci-fi", 5, ["5:00 PM", "9:00 PM"]);
+
+var nextShowing = outerSpaceMovie.getNextShowing();
 alert(nextShowing);
 
-nextShowing = getNextShowing(forbiddenPlanetMovie);
+nextShowing = forbiddenPlanetMovie.getNextShowing();
 alert(nextShowing);
