06061d10fec6d98306c1cf7be7acb890a8557637
Hoàng Vũ <hoangvubrvt@gmail.com>
2016-03-03 19:52:53 +0700

    learned about determine my location by Google Map API

--- js/myLo.js
+++ js/myLo.js
@@ -9,12 +9,21 @@ function getMyLocation() {
 }
 
 function displayLocation(position) {
+    var ourCoords = {
+        latitude : 47.624851,
+        longitude : -122.52099
+    }
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
     var div = document.getElementById("location");
-
     div.innerHTML = "You are at latitude: "+ latitude + ", Longitude: "+longitude;
+
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + "km from the WickedlySmart HQ";
+
+    showMap(position.coords);
 }
 
 function displayError(error) {
@@ -31,4 +40,63 @@ function displayError(error) {
 
     var div = document.getElementById("location");
     div.innerHTML = errorMessage;
+}
+
+function computeDistance(startCoords, endCoords) {
+    var startLaRads = degreesToRadians(startCoords.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+
+    var destLaRads = degreesToRadians(endCoords.latitude);
+    var destLongRads = degreesToRadians(endCoords.longitude);
+
+    var Radius = 6371;
+
+    var distance = Math.acos(Math.sin(startLaRads) * Math.sin(destLaRads) +
+                             Math.cos(startLaRads) * Math.cos(destLaRads) *
+                             Math.cos(startLongRads - destLongRads)) * Radius;
+    return distance;
+}
+
+function degreesToRadians(degrees) {
+    var radians = (degrees * Math.PI)/180;
+    return radians;
+}
+
+var map;
+
+function showMap(coords) {
+    var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+    var mapOptions = {
+        zoom : 18,
+        center : googleLatAndLong,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    }
+
+    var mapDiv = document.getElementById("map");
+    map = new google.maps.Map(mapDiv, mapOptions);
+
+    var title = "My Location";
+    var content = "My Location is: "+coords.latitude + ", " +coords.longitude;
+    addMarker(map, googleLatAndLong, title, content);
+}
+
+function addMarker(map, latlong, title, content){
+    var markerOptions = {
+        position : latlong,
+        map : map,
+        title : title,
+        clickable : true
+    }
+
+    var marker = new google.maps.Marker(markerOptions);
+
+    var infoWindowOptions = {
+        content : content,
+        position : latlong
+    }
+
+    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+    google.maps.event.addListener(marker, "click", function(){
+       infoWindow.open(map);
+    });
 }
