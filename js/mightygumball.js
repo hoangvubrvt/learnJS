3daf0cb6da5ef9339f8132bb15d30523720abcdd
Hoàng Vũ <hoangvubrvt@gmail.com>
2016-03-21 23:42:17 +0700

    Learn setInterval HTML5 API, JSONP to call webservice

--- js/mightygumball.js
+++ js/mightygumball.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
     //var url = "http://127.0.0.1:64286/salesdata/salesdata.json";
     var url = "http://gumball.wickedlysmart.com/";
     var request = new XMLHttpRequest();
@@ -13,9 +13,9 @@ window.onload = function() {
     };
     
     request.send(null);
-}
+}*/
 
-function updateSales(saleData) {
+/*function updateSales(saleData) {
     var saleContainer = document.getElementById("sales");
     var sales = JSON.parse(saleData);
     
@@ -25,5 +25,43 @@ function updateSales(saleData) {
         div.setAttribute("class","sale-item");
         div.innerHTML = sale.name + " sold " + sale.sales + "gumballs";
         saleContainer.appendChild(div);
+    }
+}*/
+var lastReportTime = 0;
+window.onload = function() {
+    setInterval(handleRefresh, 3000);
+}
+
+function handleRefresh() {
+    var url = "http://gumball.wickedlysmart.com/?callback=updateSales"
+              + "&lastreporttime="+lastReportTime
+              + "&random="+(new Date()).getTime();
+
+    var newScriptElement = document.createElement("script");
+    newScriptElement.setAttribute("src",url);
+    newScriptElement.setAttribute("id","jsonp");
+
+    var oldScriptElement = document.getElementById("jsonp");
+    var headTag = document.getElementsByTagName("head")[0];
+
+    if(oldScriptElement === null) {
+        headTag.appendChild(newScriptElement);
+    }else {
+        headTag.replaceChild(newScriptElement, oldScriptElement);
+    }
+}
+function updateSales(sales) {
+    var saleContainer = document.getElementById("sales");
+
+    for(var i = 0; i < sales.length; i++) {
+        var sale = sales[i];
+        var div = document.createElement("div");
+        div.setAttribute("class","sale-item");
+        div.innerHTML = sale.name + " sold " + sale.sales + "gumballs";
+        saleContainer.appendChild(div);
+    }
+
+    if(sales.length > 0) {
+        lastReportTime = sales[sales.length - 1].time;
     }
 }
